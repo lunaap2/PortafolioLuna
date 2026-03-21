5e4bce2c114a66472d1553bceb23facd3b75ed50
Tiagozzx <lunydiaz57@gmail.com>
2026-03-20 19:12:30 -0500

    fix(astro): entorno de dev sin bugs y sync package-lock

--- mi-portafolio-astro/astro.config.mjs
+++ mi-portafolio-astro/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lunaap2.github.io',
-  base: '/PortafolioLuna',
+  base: process.env.GITHUB_ACTIONS ? '/PortafolioLuna' : '/',
   vite: {
     plugins: [tailwindcss()]
   }
